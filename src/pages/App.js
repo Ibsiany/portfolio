73a8cb03e5ebc9a7670bec1f12bcaac961efd8fe
Ibsiany <ibsianyd@gmail.com>
2021-03-03 21:44:17 -0300

    contato

--- src/pages/App.js
+++ src/pages/App.js
@@ -16,44 +16,53 @@ function App() {
 
     <div className="container">
       <img src="./home.jpg" alt="Imagem de fundo" />
-      {value ? (
-        <>
-          <div className="body-first">
-            <h1>Portfólio | Ibsiany Dias Godinho</h1>
-            <a href="https://ibisflix.vercel.app/" target="_blank"> 
-              IBISFLIX
-            </a>
-            <a href="https://moveit-ibsiany.vercel.app/" target="_blank"> 
-              Moveit (Desktop)
-            </a>
-            <button id="back" onClick={DesactiveButton}>
-              Voltar
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="body">
-            <h1>Portfólio | Ibsiany Dias Godinho</h1>
-            <a href="https://github.com/ibsiany" target="_blank"> 
-              GITHUB
-              <img src="./icons/github-logo-2.png" alt="GitHub" />
-            </a>
-            <a href="https://www.linkedin.com/in/ibsiany/" target="_blank">
-              LINKEDIN
-              <img src="./icons/linkedin-logo.png" alt="Linkedin" />
-            </a>
-            <a href="https://app.rocketseat.com.br/me/ibsiany-dias" target="_blank">
-              ROCKETSEAT
-              <img src="./icons/rocketseat-logo.png" alt="Rocketseat" />
-            </a>
-            <a onClick={ActiveButton} id="more">
-              SITES ONLINE
-              <img src="./icons/more.png" alt="Mais" />
-            </a>
-          </div>
-        </>
-      )}
+      <div className="body-container">
+        <h1>Portfólio | Ibsiany Dias Godinho</h1>
+        {value ? (
+          <>
+            <div className="body-first">
+              <a href="https://ibisflix.vercel.app/" target="_blank"> 
+                IBISFLIX
+              </a>
+              <a href="https://moveit-ibsiany.vercel.app/" target="_blank"> 
+                Moveit (Desktop)
+              </a>
+              <button id="back" onClick={DesactiveButton}>
+                Voltar
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+              <div className="body">
+                <a href="https://github.com/ibsiany" target="_blank"> 
+                  GITHUB
+                  <img src="./icons/github-logo-2.png" />
+                </a>
+                <a href="https://www.linkedin.com/in/ibsiany/" target="_blank">
+                  LINKEDIN
+                  <img src="./icons/linkedin-logo.png" />
+                </a>
+                <a href="https://app.rocketseat.com.br/me/ibsiany-dias" target="_blank">
+                  ROCKETSEAT
+                  <img src="./icons/rocketseat-logo.png" />
+                </a>
+                <a onClick={ActiveButton} id="more">
+                  SITES ONLINE
+                  <img src="./icons/more.png" alt="Mais" />
+                </a>
+              </div>
+          </>
+        )}
+      <div  className="contato" >
+        <a href="https://api.whatsapp.com/send?1=pt_BR&phone=5533987158055" target="_blank" alt="Telefone de contato">
+          <img src="./icons/whatsapp.svg" alt="Whatsapp"/>
+        </a>
+        <a href="mailto:ibsianyd@gmail.com" target="_blank" alt="E-mail de contato">
+          <img src="./icons/gmail-logo.png" alt="Whatsapp"/>
+        </a>
+      </div>
+      </div>
     </div>
   );
 }
